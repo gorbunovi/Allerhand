d0ce99c91cec3e1d00bfdd3d7f37f752d3b6f49f
Gorbunov Igor <gorbunovi@rambler.ru>
2022-03-05 11:48:35 +0400

    second commit

--- test_work/lib/presentation/pages/catalog_page.dart
+++ test_work/lib/presentation/pages/catalog_page.dart
@@ -7,7 +7,30 @@ class CatalogPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const Text('SingOut', style: TextStyle(color: Colors.black),),
+              IconButton(
+                  onPressed: (){},
+                  icon: const Icon(Icons.arrow_forward_sharp, color: Colors.black,),
+              )
+            ],
+          )
+        ],
+        backgroundColor: Colors.white,
         title: const Text('Каталог'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(15),
+
+          ),
+        ),
       ),
     );
   }
